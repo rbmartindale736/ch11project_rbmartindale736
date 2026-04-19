618cffebb1d7aa135245edb4f6046d37ba80881f
rbmartindale736 <rbmartindale736@mail.cfcc.edu>
2026-04-19 19:16:17 -0400

    Final

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chapter 11 Project</title>
+</head>
+<body>
+  <h1>Database with Stored Procedures</h1>
+  <p>Richard Martindale</p>
+  <p>April 2026</p>
+</body>
+</html>
